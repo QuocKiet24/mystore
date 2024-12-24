2298a073f7dde952067fc583c9ec16c643701580
trquockiet24 <trquockiet24@gmail.com>
2024-12-25 00:50:53 +0700

    add editcategory func

--- client/src/components/EditSubCategory.jsx
+++ client/src/components/EditSubCategory.jsx
@@ -1,53 +1,30 @@
-/* eslint-disable react/prop-types */
 import { useState } from "react";
 import { IoClose } from "react-icons/io5";
-import AxiosToastError from "../utils/AxiosToastError";
+import uploadImage from "../utils/UploadImage";
+import { useSelector } from "react-redux";
 import Axios from "../utils/Axios";
 import SummaryApi from "../common/SummaryApi";
 import toast from "react-hot-toast";
-import uploadImage from "../utils/UploadImage";
+import AxiosToastError from "../utils/AxiosToastError";
 
-const EditSubCategory = ({ close, fetchData, data }) => {
+const EditSubCategory = ({ close, data, fetchData }) => {
   const [subCategoryData, setSubCategoryData] = useState({
     _id: data._id,
     name: data.name,
     image: data.image,
     category: data.category || [],
   });
+  const allCategory = useSelector((state) => state.product.allCategory);
 
-  const [loading, setLoading] = useState(false);
-  const handleOnChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
 
-    setData((prev) => {
+    setSubCategoryData((preve) => {
       return {
-        ...prev,
+        ...preve,
         [name]: value,
       };
     });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      setLoading(true);
-      const response = await Axios({
-        ...SummaryApi.updateSubCategory,
-        data: data,
-      });
-      const { data: responseData } = response;
-
-      if (responseData.success) {
-        toast.success(responseData.message);
-        close();
-        fetchData();
-      }
-    } catch (error) {
-      AxiosToastError(error);
-    } finally {
-      setLoading(false);
-    }
   };
 
   const handleUploadSubCategoryImage = async (e) => {
@@ -56,91 +33,171 @@ const EditSubCategory = ({ close, fetchData, data }) => {
     if (!file) {
       return;
     }
-    setLoading(true);
+
     const response = await uploadImage(file);
     const { data: ImageResponse } = response;
-    setLoading(false);
 
-    setData((preve) => {
+    setSubCategoryData((preve) => {
       return {
         ...preve,
         image: ImageResponse.data.url,
       };
     });
   };
+
+  const handleRemoveCategorySelected = (categoryId) => {
+    const index = subCategoryData.category.findIndex(
+      (el) => el._id === categoryId
+    );
+    subCategoryData.category.splice(index, 1);
+    setSubCategoryData((preve) => {
+      return {
+        ...preve,
+      };
+    });
+  };
+
+  const handleSubmitSubCategory = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await Axios({
+        ...SummaryApi.updateSubCategory,
+        data: subCategoryData,
+      });
+
+      const { data: responseData } = response;
+
+      console.log("responseData", responseData);
+      if (responseData.success) {
+        toast.success(responseData.message);
+        if (close) {
+          close();
+        }
+        if (fetchData) {
+          fetchData();
+        }
+      }
+    } catch (error) {
+      AxiosToastError(error);
+    }
+  };
+
   return (
     <section className="modal">
-      <div className="bg-white max-w-4xl w-full p-4 rounded">
-        <div className="flex items-center justify-between">
-          <h1 className="text-2xl font-semibold ">Update Sub Category</h1>
-          <button onClick={close} className="w-fit block ml-auto">
+      <div className="w-full max-w-5xl bg-white p-4 rounded">
+        <div className="flex items-center justify-between gap-3">
+          <h1 className="font-semibold">Edit Sub Category</h1>
+          <button onClick={close}>
             <IoClose size={25} />
           </button>
         </div>
-        <form className="my-3 grid gap-2" onSubmit={handleSubmit}>
+        <form className="my-3 grid gap-3" onSubmit={handleSubmitSubCategory}>
           <div className="grid gap-1">
-            <label id="categoryName">Name</label>
+            <label htmlFor="name">Name</label>
             <input
-              type="text"
-              id="categoryName"
-              placeholder="Enter category name"
-              value={data.name}
+              id="name"
               name="name"
-              onChange={handleOnChange}
-              className="bg-blue-50 p-2 border border-blue-100 focus-within:border-primary-200 outline-none rounded"
+              value={subCategoryData.name}
+              onChange={handleChange}
+              className="p-3 bg-blue-50 border outline-none focus-within:border-primary-200 rounded "
             />
           </div>
           <div className="grid gap-1">
             <p>Image</p>
-            <div className="flex gap-4 flex-col lg:flex-row items-center">
-              <div className="border bg-blue-50 h-36 w-full lg:w-36 flex items-center justify-center rounded">
-                {data.image ? (
+            <div className="flex flex-col lg:flex-row items-center gap-3">
+              <div className="border h-36 w-full lg:w-36 bg-blue-50 flex items-center justify-center">
+                {!subCategoryData.image ? (
+                  <p className="text-sm text-neutral-400">No Image</p>
+                ) : (
                   <img
-                    alt="category"
-                    src={data.image}
+                    alt="subCategory"
+                    src={subCategoryData.image}
                     className="w-full h-full object-scale-down"
                   />
-                ) : (
-                  <p className="text-sm text-neutral-500">No Image</p>
                 )}
               </div>
-              <label htmlFor="uploadCategoryImage">
-                <div
-                  className={`
-                            ${
-                              !data.name
-                                ? "bg-gray-300"
-                                : "border-primary-200 hover:bg-primary-100"
-                            }  
-                                px-4 py-2 rounded cursor-pointer border font-medium
-                            `}
-                >
-                  {loading ? "Uploading..." : "Upload Image"}
+              <label htmlFor="uploadSubCategoryImage">
+                <div className="px-4 py-1 border border-primary-100 text-primary-200 rounded hover:bg-primary-200 hover:text-neutral-900 cursor-pointer  ">
+                  Upload Image
                 </div>
-
                 <input
-                  disabled={!data.name}
-                  onChange={handleUploadSubCategoryImage}
                   type="file"
-                  id="uploadCategoryImage"
+                  id="uploadSubCategoryImage"
                   className="hidden"
+                  onChange={handleUploadSubCategoryImage}
                 />
               </label>
             </div>
           </div>
+          <div className="grid gap-1">
+            <label>Select Category</label>
+            <div className="border focus-within:border-primary-200 rounded">
+              {/*display value**/}
+              <div className="flex flex-wrap gap-2">
+                {subCategoryData.category.map((cat) => {
+                  return (
+                    <p
+                      key={cat._id + "selectedValue"}
+                      className="bg-white shadow-md px-1 m-1 flex items-center gap-2"
+                    >
+                      {cat.name}
+                      <div
+                        className="cursor-pointer hover:text-red-600"
+                        onClick={() => handleRemoveCategorySelected(cat._id)}
+                      >
+                        <IoClose size={20} />
+                      </div>
+                    </p>
+                  );
+                })}
+              </div>
+
+              {/*select category**/}
+              <select
+                className="w-full p-2 bg-transparent outline-none border"
+                onChange={(e) => {
+                  const value = e.target.value;
+                  const categoryDetails = allCategory.find(
+                    (el) => el._id == value
+                  );
+
+                  setSubCategoryData((preve) => {
+                    return {
+                      ...preve,
+                      category: [...preve.category, categoryDetails],
+                    };
+                  });
+                }}
+              >
+                <option value={""}>Select Category</option>
+                {allCategory.map((category) => {
+                  return (
+                    <option
+                      value={category?._id}
+                      key={category._id + "subcategory"}
+                    >
+                      {category?.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+          </div>
 
           <button
-            className={`
-                    ${
-                      data.name && data.image
-                        ? "bg-primary-200 hover:bg-primary-100"
-                        : "bg-gray-300 "
-                    }
-                    py-2    
-                    font-semibold 
-                    `}
+            className={`px-4 py-2 border
+                            ${
+                              subCategoryData?.name &&
+                              subCategoryData?.image &&
+                              subCategoryData?.category[0]
+                                ? "bg-primary-200 hover:bg-primary-100"
+                                : "bg-gray-200"
+                            }    
+                            font-semibold
+                        `}
           >
-            Update Sub Category
+            Submit
           </button>
         </form>
       </div>
